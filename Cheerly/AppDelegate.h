5d6b3a082075f1044f8c5d92fde883846c8da306
Francisco Yarad <fyarad@nixter.com>
2013-06-07 09:37:45 -0400

    Initial Commit

---
+++ Cheerly/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cheerly
+//
+//  Created by Francisco Yarad on 6/7/13.
+//  Copyright (c) 2013 Nixter, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cheerly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cheerly
+//
+//  Created by Francisco Yarad on 6/7/13.
+//  Copyright (c) 2013 Nixter, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
