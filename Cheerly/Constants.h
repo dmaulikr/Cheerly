e7f66191d8e8c6f3e708a9e50bf8bd304aaa8d9b
Francisco Yarad <fyarad@nixter.com>
2013-06-07 09:56:17 -0400

    Added Facebook and Parse files

---
+++ Cheerly/Constants.h
@@ -0,0 +1,19 @@
+//
+//  Constants.h
+//  Cheerly
+//
+//  Created by Francisco Yarad on 6/7/13.
+//  Copyright (c) 2013 Nixter, Inc. All rights reserved.
+//
+
+
+#define PARSE_APP_ID @"ten9jGIcbzOmDad3KxC8gbuzOJufRJFZpBA2xeB3"
+#define PARSE_CLIENT_KEY @"kBY146DQVxeJaQFQ9h3MDImN0334xAdWnu952j7t"
+
+
+// Frameworks
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+#import <FacebookSDK/FacebookSDK.h>
+#import <QuartzCore/QuartzCore.h>
+
